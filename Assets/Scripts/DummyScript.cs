2490332bb60fd0695167632f9e4d38f62b6c5d00
jsf99 <jamiestevenfagan@gmail.com>
2022-05-03 12:32:18 -0400

    test
    test I guess

---
+++ Assets/Scripts/DummyScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Lucky there's a Family Guy!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
